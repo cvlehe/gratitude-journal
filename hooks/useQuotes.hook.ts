da61d6659c8bf3a3d4e05288330fb5d2ddf523f8
cvlehe <cvonlehe@squarespace.com>
2026-02-18 15:08:43 -0500

    add back quote endpoint

--- hooks/useQuotes.hook.ts
+++ hooks/useQuotes.hook.ts
@@ -4,17 +4,16 @@ import { Platform } from 'react-native';
 
 const useQuotes = () => {
   const fetchQuote = useCallback(async (): Promise<Quote> => {
-    return Promise.resolve({ text: 'Test quote1', author: 'Test author1' });
-    // const url = 'https://zenquotes.io/api/today';
-    // const response = await fetch(
-    //   Platform.OS === 'web' ? `https://api.allorigins.win/get?url=${url}` : url
-    // );
-    // const data = await response.json();
-    // if (!data || data?.length < 1) {
-    //   return { text: 'Error fetching quote', author: '' };
-    // }
+    const url = 'https://zenquotes.io/api/today';
+    const response = await fetch(
+      Platform.OS === 'web' ? `https://api.allorigins.win/get?url=${url}` : url
+    );
+    const data = await response.json();
+    if (!data || data?.length < 1) {
+      return { text: 'Error fetching quote', author: '' };
+    }
 
-    // return Promise.resolve({ text: data[0].q, author: data[0].a });
+    return Promise.resolve({ text: data[0].q, author: data[0].a });
   }, []);
 
   return { fetchQuote };
